6eb4d14a1c9be7dc4ffcae8798989657b32ee296
Temitope-Akinsoto <takinsoto@gmail.com>
2019-10-02 12:51:20 -0700

    feat: Add the useDarkMode custom hook
    In this commit, I added the useDarkMode custom hook which main function is
    for toggling the dark-mode class from true to false and vice versa depending on
    the initial value

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 const useDarkMode = initialValue => {
+const [darkMode, setDarkMode] = useLocalStorage("dark-mode", initialValue);
+ useEffect(() => {
+   darkMode ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode");
+ }, [darkMode]);
+ return [darkMode, setDarkMode];
 
 }
